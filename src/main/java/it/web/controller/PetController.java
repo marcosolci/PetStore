ce2db9079d5c24f2aa5079234e9a0387700d716b
root <info@saic.it>
2017-03-12 19:53:46 +0100

    FiRst Commit

---
+++ src/main/java/it/web/controller/PetController.java
@@ -0,0 +1,48 @@
+package it.web.controller;
+
+import it.web.bean.Pet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin")
+public class PetController {
+
+    @RequestMapping(value = { "/homePage"}, method = RequestMethod.GET)
+    public ModelAndView homePage() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("homePage");
+        return model;
+    } 
+    
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public @ResponseBody List<Pet> printHello(ModelMap model) {
+
+        List<Pet> list = getList();
+
+        return list;
+
+    }
+
+    private List<Pet> getList() {
+
+        List<Pet> list = new ArrayList<Pet>();
+        Pet pet = new Pet();
+        pet.setId(1);
+        pet.setCode("p1");
+        pet.setName("name1");
+        list.add(pet);
+
+        return list;
+
+    }
+
+}
